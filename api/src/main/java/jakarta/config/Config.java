793836a2c2c4f60697ac0248c0b49e6e14feae24
Tomas Langer <tomas.langer@oracle.com>
2022-05-12 16:27:02 +0200

    Updated based on Config meeting.

--- api/src/main/java/jakarta/config/Config.java
+++ api/src/main/java/jakarta/config/Config.java
@@ -138,7 +138,10 @@ public interface Config {
      *                       but with values from the changed config source, the collection provided is the set of node
      *                       names that were modified (if this node is {@code server} and {@code server.host} was modified,
      *                       the set would contain the String {@code host}; the function returns {@code true} if further
-     *                       events should be delivered, {@code false} to stop receiving change events
+     *                       events should be delivered, {@code false} to stop receiving change events; in case an exception
+     *                       is thrown from the processor, the subscription is retained and further changes will be sent to
+     *                       the processor
+     * @throws java.lang.NullPointerException in case the changeConsumer is null
      */
     void onChange(BiFunction<Config, Collection<String>, Boolean> changeConsumer);
 
