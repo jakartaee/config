e6886a1f4babc776e3aa4f4e3dc1b8409c957638
jansupol <jan.supol@oracle.com>
2023-07-28 12:47:39 +0200

    Update Loader with methods for path and separator
    Signed-off-by: jansupol <jan.supol@oracle.com>

--- api/src/main/java/jakarta/config/Loader.java
+++ api/src/main/java/jakarta/config/Loader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Contributors to the Eclipse Foundation
+ * Copyright (c) 2022, 2023 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -23,17 +23,30 @@ import java.util.ServiceLoader;
 /**
  * A loader of configuration-related objects.
  *
- * <p>Sample usage:</p>
+ * <p>The loader resolves values of the given configuration related interface with portion of application's
+ * <em>persistent configuration</em>. The portion of the <em>persistent configuration</em> can be identified
+ * by <em>configuration path</em>.</p>
+ *
+ * <p>In the following example {@code MyConfigurationRelatedObject} is the configuration interface to be resolved,
+ * and the path {@code my.configuration} is the <em>canonical representation</em> of a <em>configuration key</em> corresponding to
+ * the configuration portion used to resolve the configuration interface.
+ * An instance of the configuration interface is created by the {@link Loader}:</p>
  *
  * <blockquote><pre>{@linkplain Loader Loader} loader = {@linkplain Loader Loader}.{@linkplain Loader#bootstrap() bootstrap()};
  *MyConfigurationRelatedObject object = null;
  *try {
- *  object = loader.{@linkplain #load(Class) load(MyConfigurationRelatedObject.class)};
+ *  object = loader.{@linkplain #path(String) path("my.configuration")}.{@linkplain #load(Class) load(MyConfigurationRelatedObject.class)};
  *} catch ({@linkplain NoSuchObjectException} noSuchObjectException) {
  *  // object is <a href="doc-files/terminology.html#absent">absent</a>
  *} catch ({@linkplain ConfigException} configException) {
  *  // a {@linkplain #load(Class) loading}-related error occurred
  *}</pre></blockquote>
+ *
+ * <p>Implementations of the methods in this class must be idempotent.</p>
+ *
+ * <p>Implementations of the methods in this class must be safe for concurrent use by multiple threads.</p>
+ *
+ * <p>Implementations of the methods in this class must not return {@code null}.</p>
  *
  * @see #bootstrap()
  *
@@ -42,6 +55,10 @@ import java.util.ServiceLoader;
  * @see #load(Class)
  *
  * @see #load(TypeToken)
+ *
+ * @see #path(String)
+ *
+ * @see #separator(String)
  *
  * @see <a href="doc-files/terminology.html">Terminology</a>
  */
@@ -55,14 +72,6 @@ public interface Loader {
      * determined whether any given configuration-related object is
      * "of the supplied {@code type}" are currently wholly
      * undefined.</p>
-     *
-     * <p>Implementations of this method must not return {@code
-     * null}.</p>
-     *
-     * <p>Implementations of this method must be idempotent.</p>
-     *
-     * <p>Implementations of this method must be safe for concurrent
-     * use by multiple threads.</p>
      *
      * <p>Implementations of this method may or may not return a <a
      * href="doc-files/terminology.html#determinate">determinate</a>
@@ -100,13 +109,6 @@ public interface Loader {
      * "of the supplied {@code type}" are currently wholly
      * undefined.</p>
      *
-     * <p>Implementations of this method must not return {@code
-     * null}.</p>
-     *
-     * <p>Implementations of this method must be idempotent.</p>
-     *
-     * <p>Implementations of this method must be safe for concurrent
-     * use by multiple threads.</p>
      *
      * <p>Implementations of this method may or may not return a <a
      * href="doc-files/terminology.html#determinate">determinate</a>
@@ -135,17 +137,52 @@ public interface Loader {
     public <T> T load(TypeToken<T> type);
 
     /**
+     * Return a new instance of a {@link Loader} with the <em>configuration path</em> set.
+     *
+     * <p>The <em>configuration path</em> is a <em>canonical representation</em> of a <em>configuration key</em>,
+     * as defined in the Jakarta Config specification.</p>
+     *
+     * <p>For instance, should the <em>persistent configuration</em> contain
+     * <pre>  my.configuration.user=tester</pre>
+     * the <em>configuration key</em> for configuration portion prefixed by
+     * {@code my.configuration} would be the prefix {@code my.configuration}.
+     * The <em>canonical representation</em> (configuration path) of this configuration key using
+     * the default configuration path separator of {@code .} (dot) would be {@code my.configuration} as well,
+     * while the <em>canonical representation</em> (path) of this configuration key using
+     * the {@code /} (slash) separator would be {@code my/configuration}.
+     * </p>
+     *
+     * @param path a {@link String} representation of a configuration path.
+     * @return a new instance of the {@link Loader} class with a configured <em>path</em>.
+     *
+     * @see Configuration path.
+     */
+    public Loader path(String path);
+
+    /**
+     * Returns a new instance of a {@link Loader} with the <em>configuration path separator</em> set.
+     *
+     * <p>The <em>configuration path separator</em> is used for defining <em>canonical representation</em> (configuration path)
+     * of a <em>configuration key</em> of the application's <em>persistent configuration</em>.</p>
+     *
+     * <p>The default separator is {@code . (dot)} symbol.</p>
+     *
+     * <p>For instance, should the <em>configuration key</em> of the <em>persistent configuration</em> be
+     * {@code my.configuration}, the corresponding <em>configuration path</em> using the default separator would be
+     * {@code my.configuration}, whereas using the {@code / (slash)} separator the configuration path would be
+     * {@code my/configuration}.</p>
+     *
+     * @param separator the configuration path separator used in configuration path.
+     *
+     * @return a new instance of a {@link Loader} with the <em>configuration path separator</em> set.
+     */
+    public Loader separator(String separator);
+
+    /**
      * <em>{@linkplain #bootstrap(ClassLoader) Bootstraps}</em> a
      * {@link Loader} instance for subsequent usage using the
      * {@linkplain Thread#getContextClassLoader() context
      * classloader}.
-     *
-     * <p>This method never returns {@code null}.</p>
-     *
-     * <p>This method is idempotent.</p>
-     *
-     * <p>This method is safe for concurrent use by multiple
-     * threads.</p>
      *
      * <p>This method may or may not return a <a
      * href="doc-files/terminology.html#determinate">determinate</a>
@@ -203,13 +240,6 @@ public interface Loader {
      * </li>
      *
      * </ol>
-     *
-     * <p>This method never returns {@code null}.</p>
-     *
-     * <p>This method is idempotent.</p>
-     *
-     * <p>This method is safe for concurrent use by multiple
-     * threads.</p>
      *
      * <p>This method may or may not return a <a
      * href="doc-files/terminology.html#determinate">determinate</a>
